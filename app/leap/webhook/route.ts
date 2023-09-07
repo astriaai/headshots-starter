c61761a98937046bda94e7f1f1495717e347de48
Marfuen <marfuen98@gmail.com>
2023-09-07 10:44:57 +0900

    user service role

--- app/leap/webhook/route.ts
+++ app/leap/webhook/route.ts
@@ -1,5 +1,6 @@
 import { Database } from '@/types/supabase';
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+import { createClient } from '@supabase/supabase-js';
 import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
@@ -8,7 +9,7 @@ export const dynamic = 'force-dynamic'
 
 const resendApiKey = process.env.RESEND_API_KEY;
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 if (!resendApiKey) {
   throw new Error("MISSING RESEND_API_KEY!");
@@ -18,7 +19,7 @@ if (!supabaseUrl) {
   throw new Error("MISSING NEXT_PUBLIC_SUPABASE_URL!");
 }
 
-if (!supabaseAnonKey) {
+if (!supabaseServiceRoleKey) {
   throw new Error("MISSING NEXT_PUBLIC_SUPABASE_ANON_KEY!");
 }
 
@@ -27,10 +28,7 @@ export async function POST(request: Request) {
   const incomingData = await request.json();
   const { state, user_id } = incomingData;
 
-  const supabase = createRouteHandlerClient<Database>({ cookies }, {
-    supabaseUrl,
-    supabaseKey: supabaseAnonKey,
-  });
+  const supabase = createClient(supabaseUrl as string, supabaseServiceRoleKey as string);
   const { data: { user } } = await supabase.auth.admin.getUserById(user_id);
 
   if (!user) {
