9b9077100ced520b509f97e9389a6398c68b284e
Marfuen <marfuen98@gmail.com>
2023-09-08 11:05:34 +0900

    fix issues with redirect

--- app/overview/models/[id]/page.tsx
+++ app/overview/models/[id]/page.tsx
@@ -1,31 +1,22 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-import ModelsTable from '@/components/ModelsTable';
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
 import Login from '@/app/login/page';
-import { redirect } from 'next/navigation';
 import { Database } from '@/types/supabase';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 export default async function Index({
   params,
-  searchParams,
 }: {
-  params: { slug: string };
-  searchParams?: { [key: string]: string | string[] | undefined };
+  params: { id: string };
 }) {
   const supabase = createServerComponentClient<Database>({ cookies });
   const { data: {user} } = await supabase.auth.getUser();
-
-  if (!searchParams?.id) {
-    redirect('/overview');
-  }
 
   if (!user) {
     return <Login />
   }
 
-  const { data: model } = await supabase.from('models').select('*').eq('id', searchParams?.id).eq("user_id", user.id).single();
+  const { data: model } = await supabase.from('models').select('*').eq('id', Number(params.id)).eq("user_id", user.id).single();
 
   if (!model) {
     redirect('/overview');
