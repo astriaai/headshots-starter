0d5ecd623ad44ec9d130bd5b60e88f721ecf2c41
Marfuen <marfuen98@gmail.com>
2023-09-08 11:14:22 +0900

    add back arrow from model page

--- app/overview/models/[id]/page.tsx
+++ app/overview/models/[id]/page.tsx
@@ -2,6 +2,7 @@ import Login from '@/app/login/page';
 import { Database } from '@/types/supabase';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
 export default async function Index({
@@ -24,8 +25,13 @@ export default async function Index({
 
   return (
     <div id="train-model-container" className="w-full h-full px-20 py-10">
-      <div className='flex flex-row w-full justify-between align-middle text-center'>
-        <h1>Viewing model {model.name}</h1>
+      <Link href="/overview">
+        <p className='font-semibold text-md'>
+          &larr; Back to overview
+        </p>
+      </Link>
+      <div className='flex flex-row w-full mt-4'>
+        <h1 className='text-3xl self-center text-center mx-auto'>{model.name}</h1>
       </div>
     </div>
   )
