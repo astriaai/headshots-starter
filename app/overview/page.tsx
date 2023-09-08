8773586c96a035596ddeb6b934998db94cf26fe5
Claudio Fuentes <imclaudfuen@gmail.com>
2023-09-08 13:45:32 +0900

    improved empty state for models table

--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -5,6 +5,7 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import Link from "next/link";
 import Login from "../login/page";
+import { FaImages } from "react-icons/fa";
 
 export default async function Index() {
   const supabase = createServerComponentClient<Database>({ cookies });
@@ -23,7 +24,10 @@ export default async function Index() {
     .eq("user_id", user.id);
 
   return (
-    <div id="train-model-container" className="w-full h-full px-20 py-10">
+    <div
+      id="train-model-container"
+      className="flex flex-grow w-full h-full px-20 py-10 justify-center items-center"
+    >
       {models && models.length > 0 && (
         <div>
           <div className="flex flex-row w-full justify-between align-middle text-center">
@@ -36,11 +40,12 @@ export default async function Index() {
         </div>
       )}
       {models && models.length === 0 && (
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-4 items-center">
+          <FaImages size={64} className="text-gray-500" />
           <h1 className="text-xl">Get started by training your first model.</h1>
           <div>
             <Link href="/overview/models/train">
-              <Button>Train model</Button>
+              <Button size={"lg"}>Train model</Button>
             </Link>
           </div>
         </div>
