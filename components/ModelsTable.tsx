a0a6cb5a8175526d8217b928007133ce2e7fbd51
Claudio Fuentes <imclaudfuen@gmail.com>
2023-09-08 10:42:24 +0900

    updated database types

--- components/ModelsTable.tsx
+++ components/ModelsTable.tsx
@@ -6,23 +6,30 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
+} from "@/components/ui/table";
 import { Database } from "@/types/supabase";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from 'next/headers';
+import { cookies } from "next/headers";
 import { Icons } from "./icons";
 
-export default async function ModelsTable() {
+export default async function ModelsTable({
+  models,
+}: {
+  models: Database.Model[];
+}) {
   const supabase = createServerComponentClient<Database>({ cookies });
   const {
-    data: { user }
-  } = await supabase.auth.getUser()
+    data: { user },
+  } = await supabase.auth.getUser();
 
   if (!user) {
     return null;
   }
 
-  const { data: models } = await supabase.from('models').select('*').eq('user_id', user.id);
+  const { data: models } = await supabase
+    .from("models")
+    .select("*")
+    .eq("user_id", user.id);
 
   return (
     <Table className="w-full">
@@ -34,14 +41,19 @@ export default async function ModelsTable() {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {models?.map((model) => (
+        {models.map((model) => (
           <TableRow key={model.id}>
             <TableCell className="font-medium">{model.name}</TableCell>
-            <TableCell>{model.status} {model.status === "processing" && <Icons.spinner className="h-4 w-4 animate-spin" />}</TableCell>
+            <TableCell>
+              {model.status}{" "}
+              {model.status === "processing" && (
+                <Icons.spinner className="h-4 w-4 animate-spin" />
+              )}
+            </TableCell>
             <TableCell>{model.type}</TableCell>
           </TableRow>
         ))}
       </TableBody>
     </Table>
-  )
+  );
 }
