18e753fedb5474d00fdb8c595d5c0b8e9f0b6ad6
Claudio Fuentes <imclaudfuen@gmail.com>
2023-09-08 18:31:42 +0900

    added real prompts

--- app/leap/train-webhook/route.ts
+++ app/leap/train-webhook/route.ts
@@ -1,9 +1,9 @@
-import { Database } from '@/types/supabase';
-import { createClient } from '@supabase/supabase-js';
-import { NextResponse } from 'next/server';
-import { Resend } from 'resend';
+import { Database } from "@/types/supabase";
+import { createClient } from "@supabase/supabase-js";
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
 
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
 
 const resendApiKey = process.env.RESEND_API_KEY;
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
@@ -41,11 +41,14 @@ export async function POST(request: Request) {
   const incomingData = await request.json();
   const { result } = incomingData;
   const urlObj = new URL(request.url);
-  const user_id = urlObj.searchParams.get('user_id');
-  const webhook_secret = urlObj.searchParams.get('webhook_secret');
+  const user_id = urlObj.searchParams.get("user_id");
+  const webhook_secret = urlObj.searchParams.get("webhook_secret");
 
   if (!webhook_secret) {
-    return NextResponse.json({}, { status: 500, statusText: "Malformed URL, no webhook_secret detected!" });
+    return NextResponse.json(
+      {},
+      { status: 500, statusText: "Malformed URL, no webhook_secret detected!" }
+    );
   }
 
   if (webhook_secret.toLowerCase() !== leapWebhookSecret?.toLowerCase()) {
@@ -53,45 +56,65 @@ export async function POST(request: Request) {
   }
 
   if (!user_id) {
-    return NextResponse.json({}, { status: 500, statusText: "Malformed URL, no user_id detected!" });
+    return NextResponse.json(
+      {},
+      { status: 500, statusText: "Malformed URL, no user_id detected!" }
+    );
   }
 
-  const supabase = createClient<Database>(supabaseUrl as string, supabaseServiceRoleKey as string, {
-    auth: {
-      autoRefreshToken: false,
-      persistSession: false,
-      detectSessionInUrl: false
+  const supabase = createClient<Database>(
+    supabaseUrl as string,
+    supabaseServiceRoleKey as string,
+    {
+      auth: {
+        autoRefreshToken: false,
+        persistSession: false,
+        detectSessionInUrl: false,
+      },
     }
-  });
-  const { data: { user }, error } = await supabase.auth.admin.getUserById(user_id);
+  );
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.admin.getUserById(user_id);
 
   if (error) {
-    return NextResponse.json({}, { status: 401, statusText: error.message, })
+    return NextResponse.json({}, { status: 401, statusText: error.message });
   }
 
   if (!user) {
-    return NextResponse.json({}, { status: 401, statusText: "User not found!" })
+    return NextResponse.json(
+      {},
+      { status: 401, statusText: "User not found!" }
+    );
   }
 
   try {
     if (result.status === "finished") {
       // Send Email
       await resend.emails.send({
-        from: 'noreply@headshots.tryleap.ai',
+        from: "noreply@headshots.tryleap.ai",
         to: user?.email ?? "",
-        subject: 'Your model was successfully trained!',
-        html: `<h2>We're writing to notify you that your model training was successful!</h2>`
+        subject: "Your model was successfully trained!",
+        html: `<h2>We're writing to notify you that your model training was successful!</h2>`,
       });
 
-      const { data: modelUpdated, error: modelUpdatedError } = await supabase.from("models").update({
-        status: "finished",
-      }).eq("modelId", result.id).select();
+      const { data: modelUpdated, error: modelUpdatedError } = await supabase
+        .from("models")
+        .update({
+          status: "finished",
+        })
+        .eq("modelId", result.id)
+        .select();
 
       if (modelUpdatedError) {
         console.log(modelUpdatedError);
-        return NextResponse.json({
-          message: "Something went wrong!"
-        }, { status: 500, statusText: "Something went wrong!" })
+        return NextResponse.json(
+          {
+            message: "Something went wrong!",
+          },
+          { status: 500, statusText: "Something went wrong!" }
+        );
       }
 
       if (!modelUpdated) {
@@ -99,40 +122,62 @@ export async function POST(request: Request) {
         console.log({ modelUpdated });
       }
 
-      const prompt = "8k portrait of professional photo, in an office setting, with a white background";
+      const prompt = [
+        "8k close up linkedin profile picture of @subject, professional jack suite, professional headshots, photo-realistic, 4k, high-resolution image, workplace settings, upper body, modern outfit, professional suit, businessman, blurred background, glass building, office window",
+        "8k linkedin professional profile photo of @subject in a suit with studio lighting, bokeh, corporate portrait headshot photograph best corporate photography photo winner, meticulous detail, hyperrealistic, centered uncropped symmetrical beautiful",
+        "realistic pencil drawing of a beautiful @subject, accurately drawn circular eyes, accurately drawn circles, black and white, sketch, pencil strokes, pencil lines, paper texture, high resolution, high resolution textures, sharp features, 32k, super-resolution, sharp focus",
+        "professional historical portrait photo of @subject from 1860, sepia tone, meticulous detail, hyperrealistic, medium long hair, facial hair, centered uncropped symmetrical , daguerreotype, framed picture",
+      ];
+
       for (let index = 0; index < 4; index++) {
-        const resp = await fetch(`https://api.tryleap.ai/api/v1/images/models/${result.id}/inferences`, {
-          method: 'POST',
-          headers: { accept: 'application/json', 'content-type': 'application/json', Authorization: `Bearer ${leapApiKey}` },
-          body: JSON.stringify({
-            prompt,
-            numberOfImages: 4,
-            webhookUrl: `${leapImageWebhookUrl}?user_id=${user.id}&model_id=${result.id}&webhook_secret=${leapWebhookSecret}&model_db_id=${modelUpdated[0]?.id}`
-          }),
-        });
+        const resp = await fetch(
+          `https://api.tryleap.ai/api/v1/images/models/${result.id}/inferences`,
+          {
+            method: "POST",
+            headers: {
+              accept: "application/json",
+              "content-type": "application/json",
+              Authorization: `Bearer ${leapApiKey}`,
+            },
+            body: JSON.stringify({
+              prompt: prompt[index],
+              numberOfImages: 4,
+              webhookUrl: `${leapImageWebhookUrl}?user_id=${user.id}&model_id=${result.id}&webhook_secret=${leapWebhookSecret}&model_db_id=${modelUpdated[0]?.id}`,
+            }),
+          }
+        );
         const { status, statusText } = resp;
         console.log({ status, statusText });
       }
     } else {
       // Send Email
       await resend.emails.send({
-        from: 'noreply@headshots.tryleap.ai',
+        from: "noreply@headshots.tryleap.ai",
         to: user?.email ?? "",
-        subject: 'Your model failed to train!',
-        html: `<h2>We're writing to notify you that your model training failed!.</h2>`
+        subject: "Your model failed to train!",
+        html: `<h2>We're writing to notify you that your model training failed!.</h2>`,
       });
 
-      await supabase.from("models").update({
-        status: "failed",
-      }).eq("modelId", result.id);
+      await supabase
+        .from("models")
+        .update({
+          status: "failed",
+        })
+        .eq("modelId", result.id);
     }
-    return NextResponse.json({
-      message: "success"
-    }, { status: 200, statusText: "Success" })
+    return NextResponse.json(
+      {
+        message: "success",
+      },
+      { status: 200, statusText: "Success" }
+    );
   } catch (e) {
     console.log(e);
-    return NextResponse.json({
-      message: "Something went wrong!"
-    }, { status: 500, statusText: "Something went wrong!" })
+    return NextResponse.json(
+      {
+        message: "Something went wrong!",
+      },
+      { status: 500, statusText: "Something went wrong!" }
+    );
   }
 }
