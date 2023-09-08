c75073fafd0d211fb86453835004b299453071c8
Claudio Fuentes <imclaudfuen@gmail.com>
2023-09-08 13:51:32 +0900

    edited landing page hero copy and positioning

--- app/overview/page.tsx
+++ app/overview/page.tsx
@@ -42,7 +42,9 @@ export default async function Index() {
       {models && models.length === 0 && (
         <div className="flex flex-col gap-4 items-center">
           <FaImages size={64} className="text-gray-500" />
-          <h1 className="text-xl">Get started by training your first model.</h1>
+          <h1 className="text-2xl">
+            Get started by training your first model.
+          </h1>
           <div>
             <Link href="/overview/models/train">
               <Button size={"lg"}>Train model</Button>
