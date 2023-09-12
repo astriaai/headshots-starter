bae41d1c3ac748e3739d14b6af909fc67116a283
Alex Schachne <alex@f.inc>
2023-09-11 21:53:34 -0700

    permanent footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
 import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "Headshots AI",
@@ -23,6 +24,7 @@ export default async function RootLayout({
         <main className="flex flex-1 flex-col items-center pb-16">
           {children}
         </main>
+        <Footer />
         <Toaster />
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default async function Index() {
   }
 
   return (
-    <div className="flex flex-col items-center h-screen pt-16">
+    <div className="flex flex-col items-center pt-16">
       <div className="flex flex-col lg:flex-row items-center gap-8 p-8 max-w-6xl w-full">
         <div className="flex flex-col space-y-4 lg:w-1/2 w-full">
           <h1 className="text-5xl font-bold">
@@ -57,7 +57,6 @@ export default async function Index() {
       </div>
       <ExplainerSection />
       <PricingSection />
-      <Footer />
     </div>
   );
 }
