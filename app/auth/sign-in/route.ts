d514d7af019f06534ab6cce854afe15265d8669c
Mariano Fuentes <marfuen98@gmail.com>
2023-09-19 11:52:18 -0700

    Enable OTP magic link

--- app/auth/sign-in/route.ts
+++ app/auth/sign-in/route.ts
@@ -8,12 +8,13 @@ export async function POST(request: Request) {
   const requestUrl = new URL(request.url)
   const formData = await request.formData()
   const email = String(formData.get('email'))
-  const password = String(formData.get('password'))
   const supabase = createRouteHandlerClient({ cookies })
 
-  const { error } = await supabase.auth.signInWithPassword({
+  const { error } = await supabase.auth.signInWithOtp({
     email,
-    password,
+    options: {
+      emailRedirectTo: requestUrl.origin,
+    }
   })
 
   if (error) {
@@ -26,8 +27,11 @@ export async function POST(request: Request) {
     )
   }
 
-  return NextResponse.redirect(requestUrl.origin, {
-    // a 301 status is required to redirect from a POST to a GET route
-    status: 301,
-  })
+  return NextResponse.redirect(
+    `${requestUrl.origin}/login?message=Check email to continue sign in process`,
+    {
+      // a 301 status is required to redirect from a POST to a GET route
+      status: 301,
+    }
+  )
 }
