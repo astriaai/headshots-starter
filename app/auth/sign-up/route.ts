0641440388216bbc19a85cd3516b67ed4fda7a10
Alex Schachne <alex@f.inc>
2023-09-25 18:39:21 -0700

    Add replit headers handling

--- app/auth/sign-up/route.ts
+++ app/auth/sign-up/route.ts
@@ -1,15 +1,16 @@
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
 
 export async function POST(request: Request) {
-  const requestUrl = new URL(request.url)
-  const formData = await request.formData()
-  const email = String(formData.get('email'))
-  const password = String(formData.get('password'))
-  const supabase = createRouteHandlerClient({ cookies })
+  const forwardedHost = request.headers.get("X-Forwarded-Host");
+  const requestUrl = new URL(forwardedHost || request.url);
+  const formData = await request.formData();
+  const email = String(formData.get("email"));
+  const password = String(formData.get("password"));
+  const supabase = createRouteHandlerClient({ cookies });
 
   const { error } = await supabase.auth.signUp({
     email,
@@ -17,7 +18,7 @@ export async function POST(request: Request) {
     options: {
       emailRedirectTo: `${requestUrl.origin}/auth/callback`,
     },
-  })
+  });
 
   if (error) {
     return NextResponse.redirect(
@@ -26,7 +27,7 @@ export async function POST(request: Request) {
         // a 301 status is required to redirect from a POST to a GET route
         status: 301,
       }
-    )
+    );
   }
 
   return NextResponse.redirect(
@@ -35,5 +36,5 @@ export async function POST(request: Request) {
       // a 301 status is required to redirect from a POST to a GET route
       status: 301,
     }
-  )
+  );
 }
