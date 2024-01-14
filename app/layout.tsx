55e36f16aec741d21afdfa72af044fa39d440c34
Anayat Khan <anayat0khan@gmail.com>
2024-01-15 00:48:13 +0530

    Setup Shadcn/ui Dark mode Theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import Footer from "@/components/Footer";
+import { ThemeProvider } from "@/components/ThemeProvider";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
@@ -12,17 +13,27 @@ export const metadata = {
 export default function RootLayout({ children }: any) {
   return (
     <html lang="en">
-      <body className="min-h-screen flex flex-col">
-        <section>
-          <Suspense fallback={<div className="flex w-full px-4 lg:px-40 py-4 items-center border-b text-center gap-8 justify-between h-[69px]" />}>
-            <Navbar />
-          </Suspense>
-        </section>
-        <main className="flex flex-1 flex-col items-center py-16">
-          {children}
-        </main>
-        <Footer />
-        <Toaster />
+      <body className="flex flex-col min-h-screen">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem = {true}
+        >
+          <section>
+            <Suspense
+              fallback={
+                <div className="flex w-full px-4 lg:px-40 py-4 items-center border-b text-center gap-8 justify-between h-[69px]" />
+              }
+            >
+              <Navbar />
+            </Suspense>
+          </section>
+          <main className="flex flex-col items-center flex-1 py-16">
+            {children}
+          </main>
+          <Footer />
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
